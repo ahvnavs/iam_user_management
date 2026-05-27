b8a36436cf1ae0bc0bb735ad865d64eb073ecb54
ahvnavs <ahvnav01@gmail.com>
2026-05-27 21:24:21 +0530

    s3 backend bucket update

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-statefile-host-s3-bucket"
-        key = "iam_user_manage/terraform.tfstate"
-        region = "ap-south-1"
-        encrypt = true
-        use_lockfile = true
-    }
+  backend "s3" {
+    bucket       = "statefile-host-bucket"
+    key          = "iam_user_manage/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }

8a1539d00143ff7e52f4c2325f291f0db14388f0
Abhinav Sharma <2210152@tcs.com>
2026-05-29 17:07:35 +0530

    add user.csv

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
     region = var.default_region
 }
-resourse "" "" {
+
+locals {
+    csv_file = csvdecode(file("${path.module}/users.csv"))
+}
+
+resource "aws_iam_user" "user_table" {
+    for_each = {for user in local.csv_file : user.Employee_ID => user}
+    name = replace(each.value.Name, " ", "")
 }
